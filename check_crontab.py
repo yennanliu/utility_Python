46bafc3c8aa182d213a877dc97ce96d208b668e3
yennanliue <f339339@gmail.com>
2017-07-31 18:25:24 +0100

    simply script with functional style

--- check_crontab.py
+++ check_crontab.py
@@ -1,31 +1,24 @@
-def get_hour(x):
-    try:
-        float(x)
-        return x 
-    except:
-        return int(datetime.now().strftime("%H"))
-    
-def get_minute(x):
-    try:
-        float(x)
-        return x 
-    except:
-        return int(datetime.now().strftime("%M"))
+
+
+from datetime import datetime
+
 
 def get_hr_min(x):
+    # get hour, minute from crontab
     hour,minute = x.split(" ")[:2][1], x.split(" ")[:2][0]
-    #print (hour,minute)
+    current_hr, current_min = datetime.now().strftime("%H"), datetime.now().strftime("%M")
+    # if crontab once in specific time, return crontab runtime 
     if hour != '*'  and minute != '*':
         return int(hour) , int(minute)
+    # if crontab run once every hour, return crontab hour and current time minute
     if (hour != '*') and (minute == '*'):
-        return int(hour), int(datetime.now().strftime("%M"))
+        return int(hour), int(current_min)
+    # if crontab run once hourly in specific minute , return crontab minute and current time hour
     if (hour == '*') and (minute != '*'):
-        return int(datetime.now().strftime("%H")),int(minute)
+        return int(current_hr),int(minute)
+    # if crontab run once every minute, return current hour and current minute 
     if (hour == '*') and (minute == '*'):
-        return int(datetime.now().strftime("%H")), int(datetime.now().strftime("%M"))
-
-
-#=============================
+        return int(current_hr), int(current_min)
 
 
 def check_cron(cronjobs):
@@ -36,15 +29,26 @@ def check_cron(cronjobs):
         currenttime= hour_now*60 + min_now 
         # get crontab time 
         hour,minute = get_hr_min(cronjob)
+        cron_content = cronjob.split(" ")[2]
         crontime = int(minute) + int(hour)*60
         if currenttime - crontime > 0:
-            print (hour,':',minute,'tomorrow',cronjob.split(" ")[2])
+            yield '{}:{} tomorrow {}'.format(hour,minute,cron_content)
         if currenttime - crontime < 0:
-            print (hour,':',minute ,'today',cronjob.split(" ")[2])
+            yield '{}:{} today {}'.format(hour,minute,cron_content)
         if currenttime - crontime == 0:
-            print (hour_now,':',min_now,'now',cronjob.split(" ")[2])
+            yield '{}:{} today {}'.format(hour_now,min_now,cron_content)
+ 
 
-                               
+#=============================
+
+def pipeline(cronjobs):
+    pipeline_ = check_cron(cronjobs)     
+    for cron in pipeline_:
+        print (cron)
+
+
+
+                      
 #=============================
 """
 
