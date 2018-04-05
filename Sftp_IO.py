ad3399c1313f9041d7f3e848ec26e13a85078b07
yennanliu <f339339@gmail.com>
2018-04-05 13:02:32 +0100

    add SFTP upload help script

---
+++ Sftp_IO.py
@@ -0,0 +1,54 @@
+# python 3 
+
+
+import pandas as pd
+from ftplib import FTP
+import pysftp
+import os
+
+
+
+
+
+# help function 
+# -------------------
+
+def access_to_sftp(file_path,host,username,private_key):
+
+    srv = pysftp.Connection(host=host, username=username, private_key=private_key)
+    # get current directory
+    print ('--- get current directory ---')
+    print (srv)
+    print (srv.listdir())
+    print (srv.pwd)
+    # move to demo directory 
+    print ('--- move to demo directory  ---')
+    srv.chdir('demo')
+    print (srv.pwd)
+    # close the connection 
+    # srv.close()
+    return srv
+
+
+
+def upload_to_sftp(connection,localpath):
+    # put(localpath, remotepath=None, callback=None, confirm=True, preserve_mtime=False)
+    try :
+        connection.put(localpath=localpath, remotepath=None, callback=None, confirm=True, preserve_mtime=False)
+        print ('dump file to SFTP success')
+    except Exception as e:
+        print ('dump job failed')
+        print ('e')
+    connection.close()
+    pass
+
+
+
+# -------------------
+
+
+
+
+
+
+
