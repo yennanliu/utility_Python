1fdb0c3263bee0b735e3ddb077dcb15c2885fe1b
yennanliu <f339339@gmail.com>
2018-09-28 17:12:46 +0100

    update code

--- multiprocess_df_demo.py
+++ multiprocess_df_demo.py
@@ -4,7 +4,9 @@
 """
 
 modify from 
+
 http://yaoyao.codes/python/2018/01/23/pandas-split-a-dataframe-into-chunks
+https://stackoverflow.com/questions/36587211/easiest-way-to-read-csv-files-with-multiprocessing-in-pandas
 
 """
 
@@ -14,6 +16,7 @@ import pandas as pd
 import numpy as np 
 from multiprocessing import Pool
 from sklearn import datasets
+
 
 
 # make toy df 
@@ -32,21 +35,21 @@ def split(df, chunk_size):
     return np.split(df, indices)
 
 
-def print_df(df_name):
-    print (df.head())
+def print_df(df):
+    print (df)
     #return pd.read_csv(filename)
 
-def main():
+def main(df):
+    
     # set up your pool
     pool = Pool(processes=4) # or whatever your hardware can support
-
-    # get a list of file names
-    #files = os.listdir('.')
-    #file_list = [filename for filename in files if filename.split('.')[1]=='csv']
-    df_list = [df_split for df_split in df if filename.split('.')[1]=='csv']
+    
+    # get a list of df names   
+    df_list = [df_split for df_split in split(df,3) ]
 
     # have your pool map the file names to dataframes
     df_list = pool.map(print_df, df)
-
+    print ('df_list : ', df_list)
     # reduce the list of dataframes to a single dataframe
     combined_df = pd.concat(df_list, ignore_index=True)
+
