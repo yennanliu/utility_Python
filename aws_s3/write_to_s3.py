36e87b1181c23bfb8d02641aa0924a0dd0d0163b
yennanliue <f339339@gmail.com>
2018-12-16 07:46:22 +0000

    add OK to use func

--- aws_s3/write_to_s3.py
+++ aws_s3/write_to_s3.py
@@ -3,9 +3,36 @@ import json
 import datetime
 import uuid
 from pytz import timezone
+import os 
+
+try:
+  AWS_KEY_ID = os.environ['AWS_KEY_ID']
+  AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+  bucketname = os.environ['bucketname']
+except:
+  print ('No S3 credential loaded')
 
 
 
+# ok to use 
+def save_to_S3(region_name,finename,bucketname):
+  # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html?highlight=upload#S3.Client.upload_file
+  s3 = boto3.resource('s3',
+                      region_name = region_name,
+                      aws_access_key_id=AWS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_KEY)
+  bucket = s3.Bucket(bucketname)
+
+  try:
+    s3.meta.client.upload_file(finename, bucketname, finename)
+    print ('upload to S3 OK')
+  except Exception as e:
+    print (e)
+    print ('upload failed')
+
+
+
+# dev 
 def write_to_s3(log_data, folder_name, model_name,region_name,aws_access_key_id,aws_secret_access_key,bucket_name):
     try:
         s3 = boto3.resource('s3', region_name=region_name,
