5a7cf05065e9b76ced32b7627294324885501a92
yennanliu <f339339@gmail.com>
2018-12-19 16:23:10 +0000

    add python super method demo

---
+++ python_super_refer_parent_demo.py
@@ -0,0 +1,32 @@
+# python 3 
+# https://www.programiz.com/python-programming/methods/built-in/super
+class Animal:
+  def __init__(self, animalName):
+    print(animalName, 'is an animal.');
+
+class Mammal(Animal):
+  def __init__(self, mammalName):
+    print(mammalName, 'is a warm-blooded animal.')
+    super().__init__(mammalName)
+    
+class NonWingedMammal(Mammal):
+  def __init__(self, NonWingedMammalName):
+    print(NonWingedMammalName, "can't fly.")
+    super().__init__(NonWingedMammalName)
+
+class NonMarineMammal(Mammal):
+  def __init__(self, NonMarineMammalName):
+    print(NonMarineMammalName, "can't swim.")
+    super().__init__(NonMarineMammalName)
+
+class Dog(NonMarineMammal, NonWingedMammal):
+  def __init__(self):
+    print('Dog has 4 legs.');
+    super().__init__('Dog')
+    
+
+
+# run as demo 
+# >>d = Dog()
+# >>print('')
+# >>bat = NonMarineMammal('Bat') 
