0641df893a143ac682daf85255aacc09bb2e781a
yennanliu <f339339@gmail.com>
2019-01-08 16:02:04 +0000

    add merge csvs and save demo

---
+++ merge_save_csvs.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import glob
+
+files = glob.glob('20*.csv')
+print (files)
+df_final = pd.concat([pd.read_csv(f, index_col=0, header=None) for f in files], keys=files)
+# save to csv 
+#df_final.to_csv('df_final.csv')
+# save to compression file 
+df_final.to_csv('df_final_gz.gz', compression='gzip')
+
