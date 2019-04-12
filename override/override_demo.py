6575ee93d506c6a292d7577703df310ece44fcd3
yennanliue <f339339@gmail.com>
2019-04-12 09:01:04 +0100

    add python override demo

---
+++ override/override_demo.py
@@ -0,0 +1,20 @@
+# https://pythonprogramminglanguage.com/overriding-methods/
+class Robot:
+    def action(self):
+        print('Robot action')
+
+class HelloRobot(Robot):
+    def action(self):
+        print('Hello world (override)')
+
+class DummyRobot(Robot):
+    def start(self):
+        print('Started.')
+
+r = HelloRobot()
+d = DummyRobot()
+
+# run the override "action"
+r.action()
+# run the orignal "action", no override 
+d.action()
