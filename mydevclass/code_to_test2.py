b9840698752a54344b38aa5c4a33b47378cb87ab
yennanliu <f339339@gmail.com>
2020-01-05 23:04:37 +0800

    add code_to_test2,py and update unit_test.py to test it

---
+++ mydevclass/code_to_test2.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def main():
+    table_name = 'some_table'
+    sqlite_db = sqlite3.connect('database.sqlite')
+    sqlite_db.execute("select * from {};".format(table_name))
+    # next, get some data and then save to db
+
+if __name__ == '__main__':
+    main()
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,16 +2,25 @@ import unittest
 import sys
 sys.path.append(".")
 sys.path.append("./mydevclass")
-from mydevclass import code_to_test
+from mydevclass import code_to_test, code_to_test2
 import mock
 
-class CodeToTestTestCase(unittest.TestCase):
+class Test_CodeToTest_1(unittest.TestCase):
     @mock.patch('code_to_test.sqlite3.connect')
     def test_database_drop_table_call(self, mock_sqlite3_connect):
         sqlite_execute_mock = mock.Mock()
         mock_sqlite3_connect.return_value = sqlite_execute_mock
         code_to_test.main()
         call = 'drop table if exists some_table;'
+        sqlite_execute_mock.execute.assert_called_with(call)
+
+class Test_CodeToTest_2(unittest.TestCase):
+    @mock.patch('code_to_test2.sqlite3.connect')
+    def test_database_drop_table_call2(self, mock_sqlite3_connect):
+        sqlite_execute_mock = mock.Mock()
+        mock_sqlite3_connect.return_value = sqlite_execute_mock
+        code_to_test2.main()
+        call = 'select * from some_table;'
         sqlite_execute_mock.execute.assert_called_with(call)
 
 if __name__ == '__main__':
