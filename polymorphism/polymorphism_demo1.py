6e53368c18df447710a8dcb5e9f7c4c58d352243
yennanliu <f339339@gmail.com>
2020-02-04 22:54:26 +0800

    update code with "calling original class method" (polymorphism_demo1.py)

--- polymorphism/polymorphism_demo1.py
+++ polymorphism/polymorphism_demo1.py
@@ -3,6 +3,9 @@ class User():
     def sign_in(self):
         print ("logged in")
 
+    def attack(self):
+        print ("do nothing")
+
 # children class
 class Wizard(User):  ### inheritance via this method ###
     def __init__(self, name, power):
@@ -20,9 +23,23 @@ class Knight(User):   ### inheritance via this method ###
     def attack(self):
         print ("attack with arrows = {}".format(self.arrows))
 
+class Testuser(User): ### inheritance via this method ###
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power 
+
+    def attack(self):
+        """
+        this User.attack(self) will use the method in the orignial class (User()), 
+        but not the one in this class (Testuser)
+        this is one of the "polymorphism" properties in OOP
+        """
+        User.attack(self) 
+
 
 Wizard1 = Wizard("Merlin", 50)
 Knight1 = Knight("Bob", 100)
+Test1 = Testuser("test_account", 100)
 
 # -----------------
 # polymorphism 
@@ -36,3 +53,4 @@ def player_attack(char):
 
 player_attack(Wizard1)
 player_attack(Knight1)
+player_attack(Test1)
