c3cc28b271f148bbb30a64550d4f2603763d6d52
yennanliu <f339339@gmail.com>
2020-02-06 09:56:46 +0800

    add python super method demo

---
+++ super/super_demo1.py
@@ -0,0 +1,33 @@
+class User:
+    def __init__(self, email):
+        self.email = email
+
+    def sign_in(self):
+        print ("logged in")
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        # 1st way to use init from  original class's (User) method via this inheritance
+        User.__init__(self, email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print ("attack with power = {}".format(self.power))
+
+class Knight(User):
+    def __init__(self, name, power, email):
+        # --------------------------
+        # Via the "Super" syntax, we have our 2nd way to use init from  original class's (User) method 
+        # --------------------------
+        super().__init__(email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print ("attack with power = {}".format(self.power))
+
+wizard1 = Wizard("jim", "100", "xxx@gmail.com")
+print (wizard1.email)
+knight1 = Knight("tom", "999", "yyy@gmail.com")
+print (knight1.email)
