8bb4d2ce9ed9618015b9915fde877bb3b52b6995
yennanliu <f339339@gmail.com>
2020-02-08 10:06:49 +0800

    add hight_order_func.py

---
+++ decorator/hight_order_func.py
@@ -0,0 +1,16 @@
+"""
+hight order function : a function which can 
+	1) accept an input as a function
+	2) return function as an output
+
+"""
+
+# function as input 
+def greet(func):
+	func 
+
+# function as output 
+def greet2():
+	def func():
+		return 5 
+	return func
