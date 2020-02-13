2906a78adf8c491736fdb7ae1ab55163a996b96b
yennanliu <f339339@gmail.com>
2020-02-13 09:42:12 +0800

    add decorator_auth_demo.py

---
+++ decorator/decorator_auth_demo.py
@@ -0,0 +1,18 @@
+# https://repl.it/@aneagoie/decorators-1
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    'valid': True
+}
+
+def authenticated(fn):
+  def wrapper(*args, **kwargs):
+    if args[0]['valid']:
+        return fn(*args, **kwargs)
+  return wrapper
+
+@authenticated
+def message_friends(user):
+    print('message has been sent')
+
+message_friends(user1)
