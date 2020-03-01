82e563f44eb64838317d1a77b9f96197898276ee
yennanliu <f339339@gmail.com>
2020-03-01 18:19:04 +0800

    add /list_set_dict_comprehension

---
+++ list_set_dict_comprehension/dict_comprehension_demo1.py
@@ -0,0 +1,19 @@
+"""
+Python dict comprehension demo 1 
+
+comprehension can make code simple/elegent, but beware of the tradeoff between readability and simplicity 
+"""
+sample_dict = {
+	
+	'a' : 1,
+	'b' : 2,
+	'c' : 3
+}
+
+my_dict1 = { key:value for key, value in sample_dict.items()}
+my_dict2 = { key:value**2 for key, value in sample_dict.items()}
+my_dict3 = { key:value  for key, value in sample_dict.items() if value%2==0}
+
+print (my_dict1)
+print (my_dict2)
+print (my_dict3)
