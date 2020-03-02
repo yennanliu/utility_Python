2831107417cada89984c7a0328f48b374a49fe1d
yennanliu <f339339@gmail.com>
2020-03-02 09:20:10 +0800

    add code comment, fix layout (pdb_demo1.py)

--- py_debugger/pdb_demo1.py
+++ py_debugger/pdb_demo1.py
@@ -5,8 +5,17 @@ https://docs.python.org/3/library/pdb.html
 import pdb
 
 def add(num1, num2):
-	pdb.set_trace()
-	return num1 + num2 
+    pdb.set_trace()
+    return num1 + num2 
 
 if __name__ == '__main__':
-	add(4, "abcvswfwe")
+    """
+    pdb useful command 
+    
+    help
+    a 
+    next
+    list
+    step
+    """
+    add(4, "abcvswfwe")
