ddc7a73206f1a576fc32f393c156a46365aecad5
yennanliu <f339339@gmail.com>
2020-03-15 23:12:40 +0800

    add unit-test test_unit_test_5.py

---
+++ tests/test_unit_test_5.py
@@ -0,0 +1,34 @@
+from requests.exceptions import Timeout
+from unittest.mock import Mock
+import unittest
+
+# Mock requests to control its behavior
+requests = Mock()
+
+def get_holidays():
+    r = requests.get('http://localhost/api/holidays')
+    if r.status_code == 200:
+        return r.json()
+    return None
+
+class TestCalendar(unittest.TestCase):
+    def test_get_holidays_retry(self):
+        # Create a new Mock to imitate a Response
+        response_mock = Mock()
+        response_mock.status_code = 200
+        response_mock.json.return_value = {
+            '12/25': 'Christmas',
+            '7/4': 'Independence Day',
+        }
+        # Set the side effect of .get()
+        requests.get.side_effect = [Timeout, response_mock]
+        # Test that the first request raises a Timeout
+        with self.assertRaises(Timeout):
+            get_holidays()
+        # Now retry, expecting a successful response
+        assert get_holidays()['12/25'] == 'Christmas'
+        # Finally, assert .get() was called twice
+        assert requests.get.call_count == 2
+
+if __name__ == '__main__':
+    unittest.main()
