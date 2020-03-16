5244cf50aecaf7f5d1ceab0127e62471f7d04a2d
yennanliu <f339339@gmail.com>
2020-03-16 14:55:29 +0800

    add tests/test_unit_test_6.py and its to-test script (tests/my_calendar.py)

---
+++ tests/my_calendar.py
@@ -0,0 +1,14 @@
+import requests
+from datetime import datetime
+
+def is_weekday():
+    today = datetime.today()
+    # Python's datetime library treats Monday as 0 and Sunday as 6
+    return (0 <= today.weekday() < 5)
+
+def get_holidays():
+    r = requests.get('https://www.google.com/')
+    print (r)
+    if r.status_code == 200:
+        return r.json()
+    return None
---
+++ tests/test_unit_test_6.py
@@ -0,0 +1,15 @@
+import unittest
+from my_calendar import get_holidays
+from requests.exceptions import Timeout
+from unittest.mock import patch
+
+class TestCalendar(unittest.TestCase):
+    def test_get_holidays_timeout(self):
+        with patch('my_calendar.requests') as mock_requests:
+            mock_requests.get.side_effect = Timeout
+            with self.assertRaises(Timeout):
+                get_holidays()
+                mock_requests.get.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
