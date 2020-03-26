113681181e008d81eb9819839f56d48885280d74
yennanliu <f339339@gmail.com>
2020-03-26 23:04:34 +0800

    fix code layout (test_unit_test_11.py)

--- tests/test_unit_test_11.py
+++ tests/test_unit_test_11.py
@@ -3,8 +3,8 @@
 
 same test as test_unit_test_10.py with different form (patch() as a Context Manager)
 some reasons when do in this way:
-	- You only want to mock an object for a part of the test scope.
-	- You are already using too many decorators or parameters, which hurts your test’s readability.
+    - You only want to mock an object for a part of the test scope.
+    - You are already using too many decorators or parameters, which hurts your test’s readability.
 """
 import unittest
 from unittest.mock import patch, Mock
@@ -15,13 +15,13 @@ from sqlalchemy.exc import DatabaseError
 class TestMyFunc(unittest.TestCase):
 
     def test_get_db_conn(self):
-    	with patch('my_db_utils.sqlalchemy') as mock_sqlalchemy:
-	        mock_sqlalchemy.create_engine.side_effect = DatabaseError
-	        mock_sqlalchemy.get_conn.return_value = "db_conn"
-	        get_db_conn()
-	        
-	        mock_sqlalchemy.create_engine.assert_called_once()
-	        assert mock_sqlalchemy.get_conn() == "db_conn"
+        with patch('my_db_utils.sqlalchemy') as mock_sqlalchemy:
+            mock_sqlalchemy.create_engine.side_effect = DatabaseError
+            mock_sqlalchemy.get_conn.return_value = "db_conn"
+            get_db_conn()
+            
+            mock_sqlalchemy.create_engine.assert_called_once()
+            assert mock_sqlalchemy.get_conn() == "db_conn"
 
 
 if __name__ == '__main__':
