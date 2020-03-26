0a08ee90bba61e7a8354dbda42c074c2546d6bf7
yennanliu <f339339@gmail.com>
2020-03-26 11:02:55 +0800

    backup unit test code

--- tests/my_db_utils.py
+++ tests/my_db_utils.py
@@ -2,10 +2,18 @@ import sqlalchemy
 from datetime import datetime
 
 def get_db_conn():
-    conn = sqlalchemy.create_engine(
-         {"db":"my_db", 
-          "user":"my_user", 
-          "password":"my_password"
-         }
-        )
-    return conn
+    try:
+        conn = sqlalchemy.create_engine("db_url")
+    except Exception as e:
+        print (e)
+
+    if str(sqlalchemy.create_engine) == '<function create_engine at 0x104695620>':
+        return sqlalchemy
+
+    # conn = sqlalchemy.create_engine(
+    #      {"db":"my_db", 
+    #       "user":"my_user", 
+    #       "password":"my_password"
+    #      }
+    #     )
+    #return conn
--- tests/test_unit_test_10.py
+++ tests/test_unit_test_10.py
@@ -1,16 +1,19 @@
 import unittest
 from unittest.mock import patch, Mock
 from my_db_utils import get_db_conn, sqlalchemy
+from sqlalchemy.exc import DatabaseError
 
 
 class TestMyFunc(unittest.TestCase):
 
-    @patch(my_db_utils.sqlalchemy)
+    @patch('my_db_utils.sqlalchemy')
     def test_my_call_api_func(self, mock_sqlalchemy):
-        mock_sqlalchemy.create_engine.side_effect = OK
-        with self.assertRaises(OK):
-            get_db_conn()
-            mock_sqlalchemy.get.assert_called_once()
+        mock_sqlalchemy.create_engine.side_effect = DatabaseError
+        #with self.assertRaises(DatabaseError):
+            #get_db_conn("abc", "def", "ijk")
+        get_db_conn()
+        mock_sqlalchemy.create_engine.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
