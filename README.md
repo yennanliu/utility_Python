864c5fab7559aaa5228f3cf64de2c26d06cc979c
yennanliu <f339339@gmail.com>
2020-04-01 16:45:20 +0800

    add interface file and its ref

--- README.md
+++ README.md
@@ -51,3 +51,7 @@
 	- https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock.side_effect
 		- A `.return_value` defines what the method will return
 		- A `.side_effect` defines what happens when you call the mocked function.
+
+- Python `interface` intro
+	- https://realpython.com/python-interface/
+
---
+++ interface/interface_demo2.py
@@ -0,0 +1,34 @@
+# https://realpython.com/python-interface/
+
+class InformalParserInterface:
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Load in the file for extracting text."""
+        pass
+
+    def extract_text(self, full_file_name: str) -> dict:
+        """Extract text from the currently loaded file."""
+        pass
+
+# pdf 
+class PdfParserNew:
+    """Extract text from a PDF."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides UpdatedInformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text(self, full_file_path: str) -> dict:
+        """Overrides UpdatedInformalParserInterface.extract_text()"""
+        pass
+
+# email
+class EmlParserNew:
+    """Extract text from an email."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides UpdatedInformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text_from_email(self, full_file_path: str) -> dict:
+        """A method defined only in EmlParser.
+        Does not override UpdatedInformalParserInterface.extract_text()
+        """
+        pass
