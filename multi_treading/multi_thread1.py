fc8b6227ed5cc0bb5108fd48259952c707e2e7c6
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-04-13 16:03:25 +0800

    add multi_treading/

---
+++ multi_treading/multi_thread1.py
@@ -0,0 +1,21 @@
+# https://realpython.com/intro-to-python-threading/
+
+import logging
+import threading
+import time
+
+import concurrent.futures
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(thread_function, range(30))
