bdcb77a2886f2150013a3279be8d4450629d3a9b
yennj12 <f339339@gmail.com>
2021-08-13 10:33:45 +0800

    Update pass_file_via_python_httpserver.py

--- hack/pass_file_via_python_httpserver.py
+++ hack/pass_file_via_python_httpserver.py
@@ -1,6 +1,11 @@
 # go to the server & directory you wanna share files from
 # axample : cd KafkaHelloWorld
+# python 3
 python -m http.server 18888
+
+# python 2
+# https://stackoverflow.com/questions/24444343/no-module-named-http-server/43939794
+python -m SimpleHTTPServer 18888
 
 # open the other terminal, and get the file via below
 # example : wget localhost:18888/build.sbt
