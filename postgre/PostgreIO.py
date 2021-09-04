4daa0be5643daba350a3fd5f1c6fdd7774e6d6a9
yennanliu <f339339@gmail.com>
2021-09-04 16:09:38 +0800

    update

--- postgre/PostgreIO.py
+++ postgre/PostgreIO.py
@@ -20,6 +20,8 @@ class PostgreIO:
         self.port = cfg['port']
         self.database = cfg['database']
 
+        self.rowsPerInsert = 5
+
     def connect(self):
         """
         method get conn, cursor
@@ -54,10 +56,52 @@ class PostgreIO:
                 cursor.close()
                 conn.close()
 
-    def create_table(self, schema, query):
+    def create_table(self, schema, ddl):
+        print (ddl)
+        try:
+            conn, cursor = self.connect()
+            cursor.execute(ddl)
+            conn.commit()
+            print ("create table OK")
+            return True
+        except Exception as e:
+            print ("create table failed", e)
+            return False
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def insert_table(self, query):
         pass
 
-    def insert_to_db(self, query):
+    # https://www.py4u.net/discuss/10876
+    def batch_insert_table(self, data, table_name, cols):
+        sqlrows = []
+        rowsPerInsert = self.rowsPerInsert
+        try:
+            conn, cursor = self.connect()
+
+            for row in data:
+                sqlrows += row
+                if ( len(sqlrows) / len(cols) ) % rowsPerInsert == 0:
+                    insertSQL = 'INSERT INTO "{table_name}" VALUES ' + ','.join(['(' + ','.join(valueSQL) + ')']*rowsPerInsert).format(table_name)
+                    cur.execute(insertSQL, sqlrows)
+                    conn.commit()
+                    sqlrows = []
+                    
+            insertSQL = 'INSERT INTO "{table_name}" VALUES ' + ','.join(['(' + ','.join(valueSQL) + ')']*len(sqlrows)).format(table_name)
+            cur.execute(insertSQL, sqlrows)
+            conn.commit()
+            print ("batch insert OK")
+        except Exception as e:
+            print ("batch insert failed")
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def update_table(self, table_name, query):
         pass
 
     def query_sql(self, query):
