b825ea0f7c5cad844adbd6ff6e34ef3e189ddedd
yennanliu <f339339@gmail.com>
2021-09-08 07:59:36 +0800

    fix main logic in server

--- stream/Server.py
+++ stream/Server.py
@@ -1,15 +1,20 @@
 import socket
+from threading import Thread
 
 """
 1) Ref
-https://docs.python.org/3/library/socket.html
-https://steelkiwi.com/blog/working-tcp-sockets/
 https://clay-atlas.com/blog/2019/10/15/python-chinese-tutorial-socket-tcp-ip/
 
+https://docs.python.org/3/library/socket.html
+https://steelkiwi.com/blog/working-tcp-sockets/
+
 2) Commands
-plz open the other terminal run below as producer
+plz use below curl commands send data via CLI
 nc -lk 9999
-https://github.com/yennanliu/KafkaSparkPoc/blob/main/spark/src/main/scala/com/yen/streamSocketToHDFS/streamSocketEventToHDFS.scala#L13
+
+curl -d "param1=value1&param2=value2" -X POST http://localhost:9999
+curl -d "123123" -X POST http://localhost:9999
+curl -d "HELLO WORLD" -X POST http://localhost:9999
 """
 class Server:
 
@@ -33,7 +38,6 @@ class Server:
             conn, addr = server.accept()
             clientMessage = str(conn.recv(1024), encoding='utf-8')
             print('Client message is:', clientMessage)
-
 
 if __name__ == '__main__':
     s = Server()
