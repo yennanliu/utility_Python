825093ece233708cad869dea8df45b8d960afc90
Yen Liu <yenanliu@amazon.com>
2023-05-08 17:31:53 +0100

    update

--- retry/dev1/run_with_func.py
+++ retry/dev1/run_with_func.py
@@ -12,26 +12,36 @@ OFFSET_AFTER_RUN_ETL = dt.timedelta(seconds=1)
 
 data_delta = 3
 
+@backfill_operator(max_run=10)
+def dummy_etl_func_1():
+
+    #print("dummy_etl_func_1 run")
+    global run_count
+    run_count += 1
+    logger.info(f"run_count = {run_count}")
+    return dt.timedelta(seconds=30)
+
+
 #@backfill_operator(max_run=10)
 @backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG)
 #@backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG, latest_time_for_rerun=OFFSET_AFTER_RUN_ETL)
 #@backfill_operator()
-def dummy_etl_func():
+def dummy_etl_func_2():
 
-    #print("dummy_etl_func run")
+    #print("dummy_etl_func_2 run")
     global run_count
     global data_delta
     run_count += 1
     logger.info(f"run_count = {run_count}")
-    #return dt.timedelta(seconds=10)
     data_delta -= 1
     return dt.timedelta(seconds=data_delta)
+
 
 def main():
 
     config_log()
-    #logger.debug(f"func_name: {dummy_etl_func.__name__}")
-    dummy_etl_func()
+    dummy_etl_func_1()
+    #dummy_etl_func_2()
 
 if __name__ == '__main__':
     main()
