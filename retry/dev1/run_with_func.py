8407ab5d1c4c10960d1085526890670a5cc52003
Yen Liu <yenanliu@amazon.com>
2023-05-08 17:23:38 +0100

    update

--- retry/dev1/run_with_func.py
+++ retry/dev1/run_with_func.py
@@ -6,24 +6,26 @@ from LogConfig import config_log
 logger = logging.getLogger("main")
 run_count = 0
 
+MAX_RUN = 20
+DEFAULT_DATA_LAG = dt.timedelta(seconds=1)
+OFFSET_AFTER_RUN_ETL = dt.timedelta(seconds=1)
 
-MAX_RUN = 10
-DEFAULT_DATA_LAG = dt.timedelta(seconds=10)
-OFFSET_AFTER_RUN_ETL = dt.timedelta(seconds=10)
+data_delta = 3
 
 #@backfill_operator(max_run=10)
-#@backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG)
-@backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG, latest_time_for_rerun=OFFSET_AFTER_RUN_ETL)
+@backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG)
+#@backfill_operator(max_run=MAX_RUN, min_data_lag_to_stop=DEFAULT_DATA_LAG, latest_time_for_rerun=OFFSET_AFTER_RUN_ETL)
 #@backfill_operator()
 def dummy_etl_func():
 
-	#print("dummy_etl_func run")
-	global run_count
-	run_count += 1
-	logger.info(f"run_count = {run_count}")
-	#return dt.timedelta(hours=10)
-	#return dt.timedelta(seconds=10)
-	return dt.timedelta(minutes=10)
+    #print("dummy_etl_func run")
+    global run_count
+    global data_delta
+    run_count += 1
+    logger.info(f"run_count = {run_count}")
+    #return dt.timedelta(seconds=10)
+    data_delta -= 1
+    return dt.timedelta(seconds=data_delta)
 
 def main():
 
