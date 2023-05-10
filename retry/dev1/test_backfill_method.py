2425567c418238f4e8af605bbc3a0019f721c51f
Yen Liu <yenanliu@amazon.com>
2023-05-10 19:02:46 +0100

    update

--- retry/dev1/test_backfill_method.py
+++ retry/dev1/test_backfill_method.py
@@ -4,44 +4,72 @@ from BackfillOperator import backfill_operator
 run_count_1 = 0
 run_count_2 = 0
 run_count_3 = 0
-delta_second_3 = 10
+run_count_4 = 0
+run_count_5 = 0
+delta_second_1 = 10
 
-def test_should_run_ten_times():
+def test_run_one_time_when_delay_in_range():
 
-    @backfill_operator(max_run=10)
-    def my_etl_func1():
+    @backfill_operator(max_run=1, min_data_lag_to_stop=dt.timedelta(seconds=10))
+    def my_etl_func():
 
         global run_count_1
         run_count_1 += 1
-        return dt.timedelta(seconds=30)
+        return dt.timedelta(seconds=1)
 
-    my_etl_func1()
-    assert run_count_1 == 10
+    my_etl_func()
+    assert run_count_1 == 1
 
-def test_should_run_three_times():
 
-    @backfill_operator(max_run=3)
-    def my_etl_func2():
+def test_run_ten_times_when_delay_out_of_range():
+
+    @backfill_operator(max_run=10, min_data_lag_to_stop=dt.timedelta(seconds=10))
+    def my_etl_func():
 
         global run_count_2
         run_count_2 += 1
+        return dt.timedelta(seconds=20)
+
+    my_etl_func()
+    assert run_count_2 == 10
+
+
+def test_should_run_max_ten_times_if_cant_catch_up():
+
+    @backfill_operator(max_run=10, min_data_lag_to_stop=dt.timedelta(seconds=10))
+    def my_etl_func():
+
+        global run_count_3
+        run_count_3 += 1
+        return dt.timedelta(seconds=15)
+
+    my_etl_func()
+    assert run_count_3 <= 10
+
+def test_should_run_ten_times_as_default():
+
+    @backfill_operator()
+    def my_etl_func():
+
+        global run_count_4
+        run_count_4 += 1
         return dt.timedelta(seconds=30)
 
-    my_etl_func2()
-    assert run_count_2 == 3
+    my_etl_func()
+    assert run_count_4 == 10
 
-def test_should_run_five_times():
+def test_should_run_five_times_if_can_catchup():
 
     @backfill_operator(max_run=10, 
         min_data_lag_to_stop=dt.timedelta(seconds=5), 
         latest_time_for_rerun=dt.timedelta(seconds=1 + 100))
-    def my_etl_func3():
+    def my_etl_func():
 
-        global run_count_3
-        global delta_second_3
-        run_count_3 += 1
-        delta_second_3 -= 1
-        return dt.timedelta(seconds=delta_second_3)
+        global run_count_5
+        global delta_second_1
+        run_count_5 += 1
+        delta_second_1 -= 1
+        return dt.timedelta(seconds=delta_second_1)
 
-    my_etl_func3()
-    assert run_count_3 == 5
+    my_etl_func()
+    assert run_count_5 == 5
