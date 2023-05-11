c99c788d8d2d53bba170c7f29f9474b481512039
Yen Liu <yenanliu@amazon.com>
2023-05-11 10:29:56 +0100

    update

--- retry/dev1/run_with_class.py
+++ retry/dev1/run_with_class.py
@@ -7,6 +7,9 @@ from LogConfig import config_log
 logger = logging.getLogger("main")
 run_count = 0
 run_count_2 = 0
+run_count_3 = 0
+delta_second_1 = 0
+
 
 class FakeETL1(MyFakeETL):
 
@@ -32,29 +35,52 @@ class FakeETL3(MyFakeETL):
         print(f"FakeETL3 run_etl, count = {run_count_2}")
         return super().run_etl()
 
+
+class FakeETL4(MyFakeETL):
+
+    @backfill_operator(max_run=10, min_data_lag_to_stop=dt.timedelta(seconds=5), latest_time_for_rerun=dt.timedelta(seconds=1 + 100))
+    def run_etl(self):
+
+        global run_count_3
+        global delta_second_1
+        run_count_3 += 1
+        delta_second_1 -= 1
+        print(f"run_count_3 = {run_count_3}")
+        return super().run_etl()
+
 def main():
 
     config_log()
     #etl_classes = [FakeETL1, FakeETL2]
-    etl_classes = [FakeETL3]
+    etl_classes = [FakeETL4]
     for etl_class in etl_classes:
         logger.info(f"ETL = {etl_class}")
+
         # etl = etl_class(
         #     init_data_lag=dt.timedelta(seconds=15),
         #     etl_process_time=dt.timedelta(seconds=1),
         #     offset_after_run_etl=dt.timedelta(seconds=1 + 8)
         # )
+
         # etl = etl_class(
         #     init_data_lag=dt.timedelta(seconds=14),
         #     etl_process_time=dt.timedelta(seconds=2),
         #     offset_after_run_etl=dt.timedelta(seconds=1 + 2)
         # )
 
+        # etl =  etl_class(
+        # init_data_lag=dt.timedelta(seconds=100),
+        # etl_process_time=dt.timedelta(seconds=10),
+        # offset_after_run_etl=dt.timedelta(seconds=1 + 1)
+        # )
+
         etl =  etl_class(
-        init_data_lag=dt.timedelta(seconds=100),
-        etl_process_time=dt.timedelta(seconds=10),
-        offset_after_run_etl=dt.timedelta(seconds=1 + 1)
+        init_data_lag=dt.timedelta(seconds=12),
+        etl_process_time=dt.timedelta(seconds=0.5),
+        offset_after_run_etl=dt.timedelta(seconds=2),
+        stop_threshold = 3
         )
+
 
         etl.run_etl()
 
