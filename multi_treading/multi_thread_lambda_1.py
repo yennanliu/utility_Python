60e9085804d57c33965b98670c023d9829dcaad4
Yen Liu <yenanliu@amazon.com>
2023-08-25 13:59:50 +0100

    update

--- multi_treading/multi_thread_lambda_1.py
+++ multi_treading/multi_thread_lambda_1.py
@@ -28,7 +28,7 @@ def cur_time_formatter(format):
 
 def write_to_s3(s3_client, bucket_name, key_name, json_data):
 
-    print(f"s3_client = {s3_client}, bucket_name = {bucket_name}, key_name = {key_name}, json_data = {json_data}")
+    #print(f"s3_client = {s3_client}, bucket_name = {bucket_name}, key_name = {key_name}, json_data = {json_data}")
     try:
         s3_client.put_object(Body=json.dumps(json_data), Bucket=bucket_name, Key=key_name)
     except Exception as e:
@@ -40,16 +40,16 @@ def upload_to_s3(data_id):
     print (f"Process id = {os.getpid()}, input = {input}, data_id = {data_id}")
 
     # # global count, lock
-    # _s3_client = boto3.client("s3")
-    # #for chunk in range(100):
-    #     # with lock:
-    #     #     counter.value +=1
-    # print (f"Process id = {os.getpid()}, input = {input}, thread_name = {thread_name}, counter value = {counter.value}")
-    # cur_time, cur_date = cur_time_formatter('%Y-%m-%d-%H-%M-%S'), cur_time_formatter('%Y/%m/%d')
-    # key_name = f"dev1/{cur_date}/{cur_time}-{data_id}.json"
-    # resp_data = {"val": data_id}
-    # print (f"resp_data = {resp_data}")
-    # write_to_s3(_s3_client, BUCKET_NAME, key_name, resp_data)
+    _s3_client = boto3.client("s3")
+    #for chunk in range(100):
+        # with lock:
+        #     counter.value +=1
+    #print (f"Process id = {os.getpid()}, input = {input}, thread_name = {thread_name}, counter value = {counter.value}")
+    cur_time, cur_date = cur_time_formatter('%Y-%m-%d-%H-%M-%S'), cur_time_formatter('%Y/%m/%d')
+    key_name = f"dev1/{cur_date}/{cur_time}-{data_id}.json"
+    resp_data = {"val": data_id}
+    print (f"resp_data = {resp_data}")
+    write_to_s3(_s3_client, BUCKET_NAME, key_name, resp_data)
 
 def lambda_handler(event, context):
 
